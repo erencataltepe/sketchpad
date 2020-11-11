13b662a513bdd1722552006a5373f72f86ae5a92
erencataltepe <erencataltepe@gmail.com>
2020-11-11 20:55:27 +0300

    Make grids darker when hover over.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 const gridContainer = document.getElementById("grid-container");
-const interval = 16;
+const interval = 4;
 
-grid.style.cssText = `grid-template: repeat(${interval} 1fr) / repeat(${interval} 1fr)`
+gridContainer.style.cssText = `grid-template: repeat(${interval}, 1fr) / repeat(${interval}, 1fr);`
 
 for (let i = 0; i < interval * interval; i++) {
   let grid = document.createElement("div");
@@ -10,3 +10,44 @@ for (let i = 0; i < interval * interval; i++) {
   grid.dataset.key = i;
   gridContainer.append(grid);
 }
+
+const gridCell = document.querySelectorAll(".grid-cell");
+
+gridCell.forEach(cell => {
+  cell.addEventListener("mouseover", e => {
+    makeDarker(e);
+  })
+})
+    
+
+
+function makeDarker(event) {
+  const cell = event.target;
+  const cellStyle = getComputedStyle(cell);
+  const currentBackgroundColor = getRGB(cellStyle.backgroundColor);
+  const newBackgroundColor = `rgb(${Number(currentBackgroundColor.red) - 25}
+    , ${Number(currentBackgroundColor.green) - 25}, ${Number(currentBackgroundColor.blue) - 25})`;
+
+  cell.style.backgroundColor = newBackgroundColor;
+}
+
+function setRandomColor(event) {
+  const cell = event.target;
+  const newRed = Math.floor(Math.random() * 256);
+  const newGreen = Math.floor(Math.random() * 256);
+  const newBlue = Math.floor(Math.random() * 256);
+}
+
+//Bu fonksiyon RGB color'ı extract ediyor.
+function getRGB(str){
+  let match = str.match(/rgba?\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3})\)?(?:, ?(\d(?:\.\d?))\))?/);
+  return match ? {
+    red: match[1],
+    green: match[2],
+    blue: match[3]
+  } : {};
+}
+
+const style = getComputedStyle(gridContainer);
+console.log(style.backgroundColor);
+console.log(getRGB(style.backgroundColor));
